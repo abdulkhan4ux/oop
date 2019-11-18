ecf079fdecd77bcb81e290e9228f8d5dae47bb31
abdulkhan4ux <57830683+abdulkhan4ux@users.noreply.github.com>
2019-11-19 02:16:16 +0500

    bb

---
+++ ascii value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char a;
+ cout << "Enter a character: ";
+ cin >> a;
+ cout << "ASCII Value of " << a << " is " << int(a);
+ return 0;
+}
